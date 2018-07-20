126b4f49e0cd95d70a9636ccadf54b8082c9abd8
Arturo Bravo <arturo123leyenda@gmail.com>
2018-07-19 20:07:59 -0500

    Julio18

--- app/helpers/usuarios_helper.rb
+++ app/helpers/usuarios_helper.rb
@@ -1,8 +1,7 @@
 module UsuariosHelper
 	def circle_img url,size=50
-		div = content_tag(:div,"",class:"circle cover popImage",
-			style:"height:#{size}px; width:#{size}px; background-image:url(#{url}); background-size: cover; float: left;
-")
+		div = content_tag(:div,"",class:"circle cover",
+			style:"height:#{size}px; width:#{size}px; background-image:url(#{url}); background-size: cover; ")
 		div.html_safe
 		
 	end
