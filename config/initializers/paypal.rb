77d23feeca257c09c07412baa028ac9765151418
Arturo Bravo Rovirosa <arturo123leyenda@gmail.com>
2018-08-29 12:44:30 -0500

    fix error sanbox in development paypal 3

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,5 +1,2 @@
 PayPal::SDK.load("config/paypal.yml", Rails.env)
-PayPal::SDK.logger = Logger.new(STDERR)
-
-# change log level to INFO
-PayPal::SDK.logger.level = Logger::INFO
+PayPal::SDK.logger = Rails.logger
