6abf37bfe6eec7c699a5c59d58ca6de5ea091db3
Arturo Bravo Rovirosa <arturo123leyenda@gmail.com>
2019-01-12 18:28:42 -0600

    task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901182104) do
+ActiveRecord::Schema.define(version: 20181006190715) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
